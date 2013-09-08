12ec2a83dac6495656db2bf54a4e5139f716423d
Philip Forget <philipforget@gmail.com>
2013-09-08 11:01:12 -0400

    Literally no clue what I'm doing

--- SerialCommand.cpp
+++ SerialCommand.cpp
@@ -1,36 +1,10 @@
-/**
- * SerialCommand - A Wiring/Arduino library to tokenize and parse commands
- * received over a serial port.
- * 
- * Copyright (C) 2012 Stefan Rado
- * Copyright (C) 2011 Steven Cogswell <steven.cogswell@gmail.com>
- *                    http://husks.wordpress.com
- * 
- * Version 20120522
- * 
- * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include "SerialCommand.h"
 
-/**
- * Constructor makes sure some things are set.
- */
-SerialCommand::SerialCommand()
+SerialCommand::SerialCommand(SoftwareSerial sSerial)
   : commandList(NULL),
     commandCount(0),
     defaultHandler(NULL),
-    term('\n'),           // default terminator for commands, newline character
+    term('\n'),  // default terminator for commands
     last(NULL)
 {
   strcpy(delim, " "); // strtok_r needs a null-terminated string
@@ -43,15 +17,15 @@ SerialCommand::SerialCommand()
  * to the handler function to deal with it.
  */
 void SerialCommand::addCommand(const char *command, void (*function)()) {
-  #ifdef SERIALCOMMAND_DEBUG
-    Serial.print("Adding command (");
-    Serial.print(commandCount);
-    Serial.print("): ");
-    Serial.println(command);
+  #ifdef SC_DEBUG
+    sSerial.print("Adding command (");
+    sSerial.print(commandCount);
+    sSerial.print("): ");
+    sSerial.println(command);
   #endif
 
   commandList = (SerialCommandCallback *) realloc(commandList, (commandCount + 1) * sizeof(SerialCommandCallback));
-  strncpy(commandList[commandCount].command, command, SERIALCOMMAND_MAXCOMMANDLENGTH);
+  strncpy(commandList[commandCount].command, command, SC_MAXLENGTH);
   commandList[commandCount].function = function;
   commandCount++;
 }
@@ -71,35 +45,35 @@ void SerialCommand::setDefaultHandler(void (*function)(const char *)) {
  * buffer for a prefix command, and calls handlers setup by addCommand() member
  */
 void SerialCommand::readSerial() {
-  while (Serial.available() > 0) {
-    char inChar = Serial.read();   // Read single available character, there may be more waiting
-    #ifdef SERIALCOMMAND_DEBUG
-      Serial.print(inChar);   // Echo back to serial stream
+  while (sSerial.available() > 0) {
+    char inChar = sSerial.read();   // Read single available character, there may be more waiting
+    #ifdef SC_DEBUG
+      sSerial.print(inChar);   // Echo back to serial stream
     #endif
 
     if (inChar == term) {     // Check for the terminator (default '\r') meaning end of command
-      #ifdef SERIALCOMMAND_DEBUG
-        Serial.print("Received: ");
-        Serial.println(buffer);
+      #ifdef SC_DEBUG
+        sSerial.print("Received: ");
+        sSerial.println(buffer);
       #endif
 
       char *command = strtok_r(buffer, delim, &last);   // Search for command at start of buffer
       if (command != NULL) {
         boolean matched = false;
         for (int i = 0; i < commandCount; i++) {
-          #ifdef SERIALCOMMAND_DEBUG
-            Serial.print("Comparing [");
-            Serial.print(command);
-            Serial.print("] to [");
-            Serial.print(commandList[i].command);
-            Serial.println("]");
+          #ifdef SC_DEBUG
+            sSerial.print("Comparing [");
+            sSerial.print(command);
+            sSerial.print("] to [");
+            sSerial.print(commandList[i].command);
+            sSerial.println("]");
           #endif
 
           // Compare the found command against the list of known commands for a match
-          if (strncmp(command, commandList[i].command, SERIALCOMMAND_MAXCOMMANDLENGTH) == 0) {
-            #ifdef SERIALCOMMAND_DEBUG
-              Serial.print("Matched Command: ");
-              Serial.println(command);
+          if (strncmp(command, commandList[i].command, SC_MAXLENGTH) == 0) {
+            #ifdef SC_DEBUG
+              sSerial.print("Matched Command: ");
+              sSerial.println(command);
             #endif
 
             // Execute the stored handler function for the command
@@ -115,12 +89,12 @@ void SerialCommand::readSerial() {
       clearBuffer();
     }
     else if (isprint(inChar)) {     // Only printable characters into the buffer
-      if (bufPos < SERIALCOMMAND_BUFFER) {
+      if (bufPos < SC_BUFFER) {
         buffer[bufPos++] = inChar;  // Put character into buffer
         buffer[bufPos] = '\0';      // Null terminate
       } else {
-        #ifdef SERIALCOMMAND_DEBUG
-          Serial.println("Line buffer is full - increase SERIALCOMMAND_BUFFER");
+        #ifdef SC_DEBUG
+          sSerial.println("Line buffer is full - increase SC_BUFFER");
         #endif
       }
     }
--- examples/SerialCommandExample/SerialCommandExample.pde
+++ examples/SerialCommandExample/SerialCommandExample.pde
@@ -1,83 +1,82 @@
-// Demo Code for SerialCommand Library
-// Steven Cogswell
-// May 2011
-
 #include <SerialCommand.h>
+#include <SoftwareSerial.h>
 
-#define arduinoLED 13   // Arduino LED on board
+#define arduinoLED 13 
 
-SerialCommand sCmd;     // The demo SerialCommand object
+SoftwareSerial mySerial(10,11);
+
+SerialCommand sCmd(&mySerial);
 
 void setup() {
-  pinMode(arduinoLED, OUTPUT);      // Configure the onboard LED for output
-  digitalWrite(arduinoLED, LOW);    // default to LED off
+    pinMode(arduinoLED, OUTPUT);      // Configure the onboard LED for output
+    digitalWrite(arduinoLED, LOW);    // default to LED off
 
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  // Setup callbacks for SerialCommand commands
-  sCmd.addCommand("ON",    LED_on);          // Turns LED on
-  sCmd.addCommand("OFF",   LED_off);         // Turns LED off
-  sCmd.addCommand("HELLO", sayHello);        // Echos the string argument back
-  sCmd.addCommand("P",     processCommand);  // Converts two arguments to integers and echos them back
-  sCmd.setDefaultHandler(unrecognized);      // Handler for command that isn't matched  (says "What?")
-  Serial.println("Ready");
+    // Setup callbacks for SerialCommand commands
+    sCmd.addCommand("ON",    LED_on);          // Turns LED on
+    sCmd.addCommand("OFF",   LED_off);         // Turns LED off
+    sCmd.addCommand("HELLO", sayHello);        // Echos the string argument back
+    sCmd.addCommand("P",     processCommand);  // Converts two arguments to integers and echos them back
+    sCmd.setDefaultHandler(unrecognized);      // Handler for command that isn't matched  (says "What?")
+    Serial.println("Ready");
 }
 
 void loop() {
-  sCmd.readSerial();     // We don't do much, just process serial commands
+    sCmd.readSerial();     // We don't do much, just process serial commands
 }
 
 
 void LED_on() {
-  Serial.println("LED on");
-  digitalWrite(arduinoLED, HIGH);
+    Serial.println("LED on");
+    digitalWrite(arduinoLED, HIGH);
 }
 
 void LED_off() {
-  Serial.println("LED off");
-  digitalWrite(arduinoLED, LOW);
+    Serial.println("LED off");
+    digitalWrite(arduinoLED, LOW);
 }
 
 void sayHello() {
-  char *arg;
-  arg = sCmd.next();    // Get the next argument from the SerialCommand object buffer
-  if (arg != NULL) {    // As long as it existed, take it
-    Serial.print("Hello ");
-    Serial.println(arg);
-  }
-  else {
-    Serial.println("Hello, whoever you are");
-  }
+    char *arg;
+    arg = sCmd.next();    // Get the next argument from the SerialCommand object buffer
+    if (arg != NULL) {    // As long as it existed, take it
+        Serial.print("Hello ");
+        Serial.println(arg);
+    }
+    else {
+        Serial.println("Hello, whoever you are");
+    }
 }
 
 
 void processCommand() {
-  int aNumber;
-  char *arg;
+    int aNumber;
+    char *arg;
 
-  Serial.println("We're in processCommand");
-  arg = sCmd.next();
-  if (arg != NULL) {
-    aNumber = atoi(arg);    // Converts a char string to an integer
-    Serial.print("First argument was: ");
-    Serial.println(aNumber);
-  }
-  else {
-    Serial.println("No arguments");
-  }
+    Serial.println("We're in processCommand");
+    arg = sCmd.next();
+    if (arg != NULL) {
+        aNumber = atoi(arg);    // Converts a char string to an integer
+        Serial.print("First argument was: ");
+        Serial.println(aNumber);
+    }
+    else {
+        Serial.println("No arguments");
+    }
 
-  arg = sCmd.next();
-  if (arg != NULL) {
-    aNumber = atol(arg);
-    Serial.print("Second argument was: ");
-    Serial.println(aNumber);
-  }
-  else {
-    Serial.println("No second argument");
-  }
+    arg = sCmd.next();
+    if (arg != NULL) {
+        aNumber = atol(arg);
+        Serial.print("Second argument was: ");
+        Serial.println(aNumber);
+    }
+    else {
+        Serial.println("No second argument");
+    }
 }
 
 // This gets set as the default handler, and gets called when no other command matches.
 void unrecognized(const char *command) {
-  Serial.println("What?");
+    Serial.println("What?");
 }
